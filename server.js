af2479e2a41e0d27f531c608729d02f89336a698
Luke <121516961+Afrozez@users.noreply.github.com>
2023-07-12 18:27:23 -0700

    updated server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const hbs = exphbs.create({})
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+});
