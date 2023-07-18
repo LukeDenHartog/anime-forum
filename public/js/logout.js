780d491c21e8c1b8606e72c13fbd7da8d2f7d71f
Ali Nawab <alinawab9830@gmail.com>
2023-07-17 21:38:49 -0500

    fixed login error

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
     });
   
     if (response.ok) {
-      document.location.replace('/signin');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
